9e07ec52c23a0ddec25d4193d18b734324d101b4
augusto SERGIO DOS SANTOS <augusto.santos@biomtech.corp>
2019-11-22 16:16:40 -0300

    Criando assignment1

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -4,6 +4,13 @@ import Person from './components/Person/Person';
 
 const App = (props) => {
 
+  const style ={
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px'
+  }
+
   const [personState, setPersonState] = useState({
     persons: [
       { name: 'Augusto', age: '27' },
@@ -25,12 +32,21 @@ const App = (props) => {
     });
   }
 
+const nameChangeHandler = (event) => {
+  setPersonState({persons: [
+    { name: 'Augusto', age: '27' },
+      { name: event.target.value, age: '29' },
+      { name: 'Santos', age: '28' }
+  ]});
+}
+
+
   return (
     <div className="App">
       <h1>Testando</h1>
-      <button onClick={switchNameHandler.bind(this, 'aaeHOOOOO')}>Switch</button>
-      <Person age={personState.persons[1].age} name={personState.persons[1].name}/>
-      <Person age="28" name={personState.persons[0].name} click = {switchNameHandler.bind(this, 'aaeHOOOOO!!!')}>Out Content</Person>
+      <button onClick={switchNameHandler.bind(this, 'aaeHOOOOO')} style={style}>Switch</button>
+      <Person age={personState.persons[0].age} name={personState.persons[0].name}/>
+      <Person age="28" name={personState.persons[1].name} click = {switchNameHandler.bind(this, 'aaeHOOOOO!!!') } changed = {nameChangeHandler}>Out Content</Person>
       <Person age="29" name={personState.persons[2].name}/>
     </div>
   );
--- react-guide/src/components/Person/Person.js
+++ react-guide/src/components/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import './Person.css'
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
         <p onClick={props.click}>Person Age {props.age} and Person Name {props.name}</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 }
