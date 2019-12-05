3f9f64d906025ef138f86d649c885d552fa732c0
Augusto Sergio dos Santos <augusto.santos@biomtech.com.br>
2019-12-05 15:17:58 -0300

    More routing

---
+++ routing--01-start/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import axios from '../../../axios';
+import Post from '../../../components/Post/Post'
+import { Link } from 'react-router-dom';
+
+class Posts extends Component {
+
+    state = {
+        posts: []
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id });
+    }
+
+    componentDidMount() {
+        console.log(this.props)
+        axios.get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+                this.setState({ posts: updatedPosts });
+                // console.log( response );
+            })
+            .catch(error => {
+                // console.log(error);
+                console.log(error);
+            });
+    }
+
+
+    render() {
+
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.
+            (post => {
+                return (
+                    //<Link to={'/' + post.id} key={post.id}>
+                        <Post key={post.id} 
+                            title={post.title}
+                            author={post.author}
+                            clicked={() => this.postSelectedHandler(post.id)} />
+                    //</Link>
+                );
+            });
+        }
+
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        )
+    }
+}
+
+export default Posts;
