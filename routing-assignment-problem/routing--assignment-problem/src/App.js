2f3fd8820e1d4bc1b5d0abe755309418093fd2a3
Augusto Sergio dos Santos <augusto.santos@biomtech.com.br>
2019-12-06 14:14:20 -0300

    Finished

--- routing-assignment-problem/routing--assignment-problem/src/App.js
+++ routing-assignment-problem/routing--assignment-problem/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
+import './App.css';
+import Course from './containers/Course/Course';
+import noMatch from './containers/NoMatch/NoMatch';
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <div className="App">
-        <ol style={{textAlign: 'left'}}>
+        <ol style={{ textAlign: 'left' }}>
           <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
           <li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
           <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
@@ -17,6 +21,23 @@ class App extends Component {
           <li>Add a 404 error page and render it for any unknown routes</li>
           <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
         </ol>
+
+        <header className='Nav'>
+          <nav>
+            <ul>
+              <li><NavLink to="/courses">Courses</NavLink></li>
+              <li><NavLink to="/users">Users</NavLink></li>
+            </ul>
+          </nav>
+        </header>
+
+        <Switch>
+          <Route path='/courses' exact component={Courses} />
+          <Route path='/users' component={Users} />
+          <Route path='/courses/:id' component={Course} />
+          <Redirect from = "/all-courses" to = "/courses"/>
+          <Route component = {noMatch}/>
+        </Switch>
       </div>
     );
   }
--- routing-assignment-problem/routing--assignment-problem/src/containers/Courses/Courses.js
+++ routing-assignment-problem/routing--assignment-problem/src/containers/Courses/Courses.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import './Courses.css';
 
@@ -8,20 +9,34 @@ class Courses extends Component {
             { id: 1, title: 'Angular - The Complete Guide' },
             { id: 2, title: 'Vue - The Complete Guide' },
             { id: 3, title: 'PWA - The Complete Guide' }
-        ]
+        ],
+        showCourse: false,
+        selectedCourseId: null
     }
 
-    render () {
+    onClickHandler(id) {
+        this.setState({ showCourse: true, selectedCourseId: id });
+    }
+
+    render() {
         return (
             <div>
                 <h1>Amazing Udemy Courses</h1>
+
                 <section className="Courses">
                     {
-                        this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
-                        } )
+                        this.state.courses.map(course => {
+                            return(
+                            <Link to={{pathname: this.props.match.url + "/" + course.id,
+                                search: '?title=' + course.title}} 
+                            onClick={() => this.onClickHandler(course.id)} 
+                            key={course.id}>
+                                <article className="Course"  >{course.title}</article>
+                            </Link>)
+                        })
                     }
                 </section>
+                {/* {this.state.showCourse && <Course courseId={this.state.selectedCourseId} />} */}
             </div>
         );
     }
